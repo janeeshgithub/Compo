5caa3c5550df9a5075e7cab6d5c91515ac22a31e
my name <janeeshpofficial@gmail.com>
2024-04-18 16:51:38 +0530

    k

---
+++ Flame/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="content">
+  <div className="background-highlight" />
+  <div className="page">
+    <div className="highlight" />
+    <div className="text">
+    <h1>&nbsp;BREAKING BAD</h1>
+      <p>"I DID IT FOR ME &nbsp;
+      I LIKED IT <br/>
+I WAS GOOD AT IT <br/>
+AND I WAS REALLY<br/>
+I WAS ALIVE"<br/><br/>&nbsp; -Walter White</p>
+    </div>
+    <div className="burn">
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+      <div className="flame" />
+    </div>
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default App
